8724bf2a2ea4428fd1be925957dbe81931937960
Vasily Kabunov <kabunov@gmail.com>
2023-11-29 09:18:15 +0700

    Added domain layer

---
+++ domain/src/main/kotlin/com/kabunov/showcase/domain/usecase/GetListUsecase.kt
@@ -0,0 +1,13 @@
+package com.kabunov.showcase.domain.usecase
+
+import com.kabunov.showcase.model.IrregularVerb
+import com.kabunov.showcase.data.repository.ContentRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetListUsecase @Inject constructor(private val contentRepository: ContentRepository) {
+
+    operator fun invoke(): Flow<List<IrregularVerb>> {
+        return contentRepository.getIrregularVerbs()
+    }
+}
